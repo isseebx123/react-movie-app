86cd886e67fdfd503bb4730bd54fbce7327a532a
SangHyun Lee <lsseebx38@gmail.com>
2018-01-08 00:10:33 +0900

    ssh test2

--- src/Movie.js
+++ src/Movie.js
@@ -6,7 +6,7 @@ class Movie extends Component{
     return (
       <div>
         <MoviePoster />
-        <h1>hello this is a movie, and ssh test</h1>
+        <h1>hello this is a movie, and ssh test2</h1>
       </div>
     )
   }
