6bb6034f19d1023ed9d90037d6092d9d3f7e5f85
SangHyun Lee <lsseebx38@gmail.com>
2018-01-07 23:54:51 +0900

    fin step: Creating React Components with JSX

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie.js';
 
 class App extends Component {
-  render() {
+  render() { // 모든 컴포넌트는 render()를 가짐, 이 컴포넌트가 보여주는게 무엇인가를 의미.
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Movie />
+        <Movie />
+        <Movie />
+        <Movie />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+// 리액트돔은 하나의 컴포넌트를 렌더링하고 root에 모든 컴포넌트가 출력된다.
+// 리액트돔은 리액트가 웹사이트에 나오도록 도와주는 것임
+// 리액트는 UI 라이브러리임.
+// 네이티브 앱을 만들고싶다면 readctNative
 registerServiceWorker();
